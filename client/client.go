e17c5461c9e751c7997fd0266679f912f3891c4e
joshbatley <jbatley12@gmail.com>
2020-07-03 15:33:18 +0100

    clean up

--- client/client.go
+++ client/client.go
@@ -8,20 +8,11 @@ import (
 	"strings"
 )
 
-// SpaHandler implements the http.Handler interface, so we can use it
-// to respond to HTTP requests. The path to the static directory and
-// path to the index file within that static directory are used to
-// serve the SPA in the given static directory.
-type SpaHandler struct {
-	StaticPath string
-	IndexPath  string
-}
+// Serve -
+func Serve(w http.ResponseWriter, r *http.Request) {
+	StaticPath := "./webapp/build"
+	IndexPath := "index.html"
 
-// ServeHTTP inspects the URL path to locate a file within the static dir
-// on the SPA handler. If a file is found, it will be served. If not, the
-// file located at the index path on the SPA handler will be served. This
-// is suitable behavior for serving an SPA (single page application).
-func (h SpaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// get the absolute path to prevent directory traversal
 	path := strings.Replace(r.URL.Path, "/config", "", 1)
 	path, err := filepath.Abs(path)
@@ -34,14 +25,14 @@ func (h SpaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// prepend the path with the path to the static directory
-	path = filepath.Join(h.StaticPath, path)
+	path = filepath.Join(StaticPath, path)
 
 	// check whether a file exists at the given path
 	_, err = os.Stat(path)
 	if os.IsNotExist(err) {
 		// file does not exist, serve index.html
 		log.Println("index")
-		http.ServeFile(w, r, filepath.Join(h.StaticPath, h.IndexPath))
+		http.ServeFile(w, r, filepath.Join(StaticPath, IndexPath))
 		return
 	} else if err != nil {
 		// if we got an error (that wasn't that the file doesn't exist) stating the
@@ -51,5 +42,8 @@ func (h SpaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// otherwise, use http.FileServer to serve the static dir
-	http.StripPrefix("/config/", http.FileServer(http.Dir(h.StaticPath))).ServeHTTP(w, r)
+	http.StripPrefix(
+		"/config/",
+		http.FileServer(http.Dir(StaticPath)),
+	).ServeHTTP(w, r)
 }
--- main.go
+++ main.go
@@ -5,23 +5,27 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+
 	"github.com/joshbatley/proxy/client"
 	"github.com/joshbatley/proxy/config"
+	"github.com/joshbatley/proxy/query"
 )
 
 func main() {
 	config, err := config.Load("./config.yml")
-	r := mux.NewRouter()
 
 	if err != nil {
 		panic("Config unreadable")
 	}
 
-	spa := client.SpaHandler{StaticPath: "./webapp/build", IndexPath: "index.html"}
-	r.PathPrefix("/config").Handler(spa)
+	// DB setup
+
+	r := mux.NewRouter()
+	r.HandleFunc("/config", client.Serve)
+	r.HandleFunc("/query", query.Serve)
 
 	http.Handle("/", r)
+
 	log.Println("Listing on localhosts:" + config.Port)
 	http.ListenAndServe("localhost:"+config.Port, nil)
-
 }
