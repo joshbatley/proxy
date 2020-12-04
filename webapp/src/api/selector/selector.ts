b0725a72f01ec4f0d3883661b5a73e565a0af51c
joshbatley <jbatley12@gmail.com>
2020-12-04 14:08:57 +0000

    added use-http package

--- webapp/src/api/selector/selector.ts
+++ webapp/src/api/selector/selector.ts
@@ -1,28 +1,24 @@
 import { useState } from 'react';
-import useFetch from 'hooks/useFetch';
-import routes from 'services/routes';
+import useFetch from 'use-http'
+import routes from 'services/app-settings'
 
 type Params = {
   limit: number;
-};
-
-type Data = {
-  'count': any,
-  'skip': any,
-  'limit': any,
-  'data': any,
-};
+}
 
 function useSelector({ limit }: Params) {
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(0)
 
-  const {
-    isLoading, error, data = [], fetch,
-  } = useFetch<Data>(`${routes.selector}?limit=${limit}&skip=${page * limit}`, { onMount: false });
+  const { loading, error, data = [], } = useFetch(`${routes.selector}?limit=${limit}&skip=${page * limit}`, {
+    onNewData: (acc = [], curr) => [...acc, ...curr.data],
+    perPage: limit
+  }, [page])
 
   return {
-    loading: isLoading, error, data, next: fetch,
-  };
+    loading, error, data, next() {
+      setPage(page + 1)
+    }
+  }
 }
 
 export default useSelector;
